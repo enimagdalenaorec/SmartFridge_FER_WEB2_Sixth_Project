2ceb40578df4547f5c8892bc19c2592edf2c3a86
Eni Magdalena Oreč <eni.magdalena@gmail.com>
2026-01-21 14:15:17 +0100

    Initial commit - add vite config.

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true  // ovo je da radi sa npm run dev
+      },
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'Smart Fridge',
+        short_name: 'Fridge',
+        description: 'Track food and expiry dates',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
